0578068639f1c14c03b0b4349b8a044e4b2dcd82
guseynovsamed <samadaghash@code.edu.az>
2024-02-25 10:29:55 +0400

    Update

--- HomeWork2/assets/js/script.js
+++ HomeWork2/assets/js/script.js
@@ -12,16 +12,25 @@ twit.addEventListener("click", function () {
   ttwit.classList.remove("animation");
   tfb.classList.add("animation");
   instext.classList.add("animation");
+  this.style.color = "#08a0e9";
+  fb.style.color = "black";
+  inst.style.color = "black";
 });
 
 fb.addEventListener("click", function () {
   ttwit.classList.add("animation");
   tfb.classList.remove("animation");
   instext.classList.add("animation");
+  this.style.color = "#0866ff";
+  inst.style.color = "black";
+  twit.style.color = "black";
 });
 
 inst.addEventListener("click", function () {
   ttwit.classList.add("animation");
   tfb.classList.add("animation");
   instext.classList.remove("animation");
+  this.style.color = "#dd2a7b";
+  fb.style.color = "black";
+  twit.style.color = "black";
 });
